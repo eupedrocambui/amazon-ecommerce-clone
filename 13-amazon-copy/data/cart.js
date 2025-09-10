f8ee6909cf0be1373fc36c3d74f275e20325a385
eupedrocambui <pedrocambui06@gmail.com>
2025-09-10 09:44:06 -0300

    Save cart using localStorage

--- 13-amazon-copy/data/cart.js
+++ 13-amazon-copy/data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 }, {
@@ -6,20 +7,26 @@ export let cart = [{
   quantity: 1
 }];
 
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 export function addToCart(productId, selectQuantity, matchingItem) {
   cart.forEach((item) => { 
       if (productId === item.productId) { // matching item verifier
         matchingItem = item;
       }
     })
-    if (matchingItem) { // if there is matching item
-      matchingItem.quantity += selectQuantity;
-    } else { // if there is not matching item
-      cart.push({
-        productId: productId,
-        quantity: selectQuantity
-      });
-    }
+  if (matchingItem) { // if there is matching item
+    matchingItem.quantity += selectQuantity;
+  } else { // if there is not matching item
+    cart.push({
+      productId: productId,
+      quantity: selectQuantity
+    });
+  }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -29,7 +36,8 @@ export function removeFromCart(productId) {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
-  
-  cart = newCart;
   })
+
+  cart = newCart;
+  saveToStorage();
 }
