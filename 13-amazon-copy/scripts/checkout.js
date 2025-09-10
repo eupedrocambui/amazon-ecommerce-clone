4ff3af879c5d8f064bf04bae17ae1e1d044f53bc
eupedrocambui <pedrocambui06@gmail.com>
2025-09-10 09:35:17 -0300

    Delete products from HTML when clicking 'delete'

--- 13-amazon-copy/scripts/checkout.js
+++ 13-amazon-copy/scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML += 
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingItem.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,6 +100,7 @@ document.querySelectorAll('.js-delete-button').forEach((link) => {
     link.addEventListener('click', () => {
         let productId = link.dataset.productId;
         removeFromCart(productId);
-        
+        let container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     })
 });
