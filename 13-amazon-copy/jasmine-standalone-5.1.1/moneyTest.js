039851d71ec8179276e3698d522209a765b0425c
eupedrocambui <pedrocambui06@gmail.com>
2025-09-25 22:28:14 -0300

    Add moneyTest using Jasmine

---
+++ 13-amazon-copy/jasmine-standalone-5.1.1/moneyTest.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', () => {
+    it('converts cents to dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('using 0', () => {
+        expect(formatCurrency(0)).toEqual('00.00');
+    });
+
+    it('rounding numbers up', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+
+    it('rounding numbers down', () => {
+        expect(formatCurrency(2000.4)).toEqual('20.00');
+    });
+})
