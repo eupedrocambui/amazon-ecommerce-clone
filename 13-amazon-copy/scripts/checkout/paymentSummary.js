b5a8d950ef135734d3d5943410e2024dc7ddd72c
eupedrocambui <pedrocambui06@gmail.com>
2025-10-21 21:41:58 -0300

    "Place your order" using backend

--- 13-amazon-copy/scripts/checkout/paymentSummary.js
+++ 13-amazon-copy/scripts/checkout/paymentSummary.js
@@ -1,5 +1,6 @@
 import { calculateCartQuantity, cart } from "../../data/cart.js";
 import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { addOrder } from "../../data/orders.js";
 import { products } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 
@@ -61,11 +62,36 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
     `;
 
     let paymentSummaryElem = document.querySelector('.js-payment-summary');
     paymentSummaryElem.innerHTML = paymentSummaryHTML;
+
+    // "Place your order" checkout button
+    const placeOrderElem = document.querySelector('.js-place-order');
+    placeOrderElem.addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+
+        const order = await response.json();
+        addOrder(order);
+
+      } catch (error) {
+        console.log('catch error (place your order button)');
+      }
+
+      window.location.href = 'orders.html';
+
+    });
 }
